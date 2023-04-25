8033fda787a0a9a6b25a805705029ed9360699e1
mesfint <mesfin_meng@yahoo.com>
2023-04-25 18:56:05 +0300

    Api Integrated

--- countries-filter/src/App.js
+++ countries-filter/src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
-import CountriesList from './components/CountriesList';
+import CountriesList from './components/countries-list/CountriesList';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CountryDetail from './components/CountryDetail';
+import CountryDetail from './components/country-details/CountryDetail';
+import Favorite from './components/favorite/Favorite';
+import Home from './components/Home';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   
   return (
     <div>
      <BrowserRouter>
+     <Header />
     <Routes>
-      <Route path="/" element={<CountriesList />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/countries-list" element={<CountriesList />} />
+      <Route path="/favorite" element={<Favorite />} />
       <Route path="/countries/:name" element={<CountryDetail />} />
     </Routes>
+    <Footer />
   </BrowserRouter>
 
   
